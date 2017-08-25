f21a2890b35ab3aa1a75b2e26f9857090e0f9a9d
sylwia <zaleska.syl@gmail.com>
2017-08-25 14:54:39 +0200

    Add TodoItem class with logic to mark item, display info about it and set boolean isDone

--- TodoItem.java
+++ TodoItem.java
@@ -3,5 +3,39 @@
  * 
  */
 public class TodoItem{
+    
+    String toDo;
+    boolean isDone = false;
+
+    public TodoItem(String toDo){
+        this.toDo = toDo;
+        this.isDone = false;
+    }
+
+    public boolean mark(){
+        return isDone=true;
+    }
+
+    public boolean getIsDone(){
+        return this.isDone;
+    }
+
+    public String getToDo(){
+        return this.toDo;
+    }
+
+    public String displayInfo(){
+        String msg;
+        if (this.isDone){
+            msg = "[x] " + this.toDo;
+        } else{
+            msg = "[] " + this.toDo;
+        }
+        return msg;
+    }
+
 
 }
+
+
+
